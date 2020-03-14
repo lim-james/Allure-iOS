ef73d117b9daa0825ae2d8c30b6e86830fcc7312
James Lim <jameslimbj@gmail.com>
2020-03-14 19:46:42 +0800

    Initial Commit

---
+++ Allure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Allure
+//
+//  Created by James on 14/3/20.
+//  Copyright © 2020 jams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
